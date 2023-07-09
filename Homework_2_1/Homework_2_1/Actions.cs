6db7dad7a6309c1b914753204a2eeaeff9aeda65
IvanTomash <tomashuckvanya@gmail.com>
2023-07-09 17:50:31 +0300

    Submit homework first time

---
+++ Homework_2_1/Homework_2_1/Actions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_1
+{
+    /// <summary>
+    /// Call the logger to log a message containing the text.
+    /// </summary>
+    internal class Actions
+    {
+
+        public Result FirstMethod()
+        {
+            Logger.Instance.LogInfo(LogTypes.Info, "Start method");
+            return new Result(true);
+        }
+
+        public Result SecondMethod()
+        {
+            Logger.Instance.LogInfo(LogTypes.Warning, "Skipped logic in method");
+            return new Result(true);
+        }
+
+        public Result ThirdMethod()
+        {           
+            return new Result(false, "I broke a logic");
+        }
+    }
+}
